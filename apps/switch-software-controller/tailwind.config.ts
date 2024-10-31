793e3d14f116cefaaee54e7444a83be3d06fe604
carimatics <github.carimatics@gmail.com>
2024-10-31 23:09:47 +0900

    chore: setup tailwindcss

---
+++ apps/switch-software-controller/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+import sharedConfig from "@switch-software-controller/tailwind-config";
+
+const config: Pick<Config, "presets" | "content"> = {
+  content: [
+    "./src/renderer/**/*.html",
+    "./src/renderer/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [sharedConfig],
+};
+
+export default config;
