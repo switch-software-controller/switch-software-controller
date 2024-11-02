a8ca9301f08b258b9e540f5e420395f696fc4020
carimatics <github.carimatics@gmail.com>
2024-11-02 14:48:38 +0900

    chore: update main script

--- apps/switch-software-controller/src/main/index.ts
+++ apps/switch-software-controller/src/main/index.ts
@@ -1,6 +1,11 @@
 import path from 'node:path';
-import { BrowserWindow, app } from 'electron';
+import { BrowserWindow, app, shell } from 'electron';
 import started from 'electron-squirrel-startup';
+import remote from '@electron/remote/main';
+import { platform } from './platform';
+import icon from '../../resources/icon.png?asset';
+
+remote.initialize();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -10,11 +15,26 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 670,
+    show: false,
+    ...(platform.isLinux ? { icon } : {}),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      sandbox: false,
+      nodeIntegration: true,
+      contextIsolation: false,
     },
+  });
+  remote.enable(mainWindow.webContents);
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url);
+    return { action: 'deny' };
   });
 
   // and load the index.html of the app.
@@ -31,24 +51,27 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  if (platform.isWindows) {
+    app.setAppUserModelId('com.example');
+  }
+
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  if (platform.isMacOS) {
     app.quit();
   }
 });
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- apps/switch-software-controller/src/main/types.d.ts
+++ apps/switch-software-controller/src/main/types.d.ts
@@ -1,2 +1,3 @@
+declare module '*.png?asset';
+
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string | undefined;
-declare const MAIN_WINDOW_VITE_NAME: string;
